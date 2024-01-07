c752cc51ffcc7503d0286b8922a42a111f63fe13
Raph2ll <raphael.ph2ll@gmail.com>
2024-01-07 12:59:23 -0300

    add: 7.6

--- book.py
+++ book.py
@@ -903,4 +903,40 @@ def le_estoque_csv():
 
     return f'Totais: {valorTotal + valorTotalICMS}, {valorTotal:.2f}, {valorTotalICMS:.2f}'
 print(le_estoque_csv())
-            
+#7.6 O objetivo desse programa é mostrar como a codificação conflitante em arquivos texto pode causar problemas. É incorreto misturar ANSI com UTF-8, que são as codificações mais empregadas no Brasil, de modo que o programador deve estar atento a isso. No Exercício resolvido 7.6 é apresentado um menu com as quatro opções a seguir, além da opção de sair do programa (essa opção nem será usada, veja o porquê na Figura 7.6).
+#Tenho é medo desse código
+def GravaLe(grava, le):
+    print('\n--', grava, ' para ', le, '-'*29)
+    arq = open('arquivo.txt', 'w', encoding=grava)
+    arq.write(S)
+    arq.close()
+    arq = open('arquivo.txt', 'r', encoding=le)
+    L = arq.readlines()
+    arq.close()
+    for x in L:
+        print(x)
+        print('-'*50)
+        # A execução do programa começa por aqui
+        print('\nDemonstra os conflitos de codificação de arquivos\n\n')
+        S = '''Uma boa porção de caracteres com acento
+        Maiúsculas: Á É Í Ó Ú Ã Õ Â Ê Ô À Ç
+        Minúsculas: á é í ó ú â ô â ê ô à ç'''
+        while True:
+            print('O que deseja fazer?')
+            print('para gravar ANSI e ler ANSI digite 1' )
+            print('para gravar UTF8 e ler UTF8 digite 2')
+            print('para gravar UTF8 e ler ANSI digite 3')
+            print('para gravar ANSI e ler UTF8 digite 4')
+            print('para sair digite 0')
+            opc = int(input(' ...opc = '))
+            if opc == 0:
+                break
+            elif opc == 1:
+                GravaLe('ANSI', 'ANSI')
+            elif opc ==2:
+                GravaLe('UTF-8', 'UTF-8')
+            elif opc == 3:
+                GravaLe('UTF-8', 'ANSI')
+            elif opc ==4:
+                GravaLe('ANSI', 'UTF-8')
+                print('Fim do Programa')
